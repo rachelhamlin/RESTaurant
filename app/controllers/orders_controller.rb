b0955a08122703a9658b0b274c4189e7008f0eb5
Rachel Hamlin <rachelmhamlin@gmail.com>
2016-04-22 11:39:22 -0400

    updates orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,16 +9,18 @@ class OrdersController < ApplicationController
   end
 
   def create
-    params = order_params
+    order_params.select{|item| item[:quantity] > 0}
 
-    new_order = Order.create order_params
+    Order.create order_params
+
+    # order_params is a hash - [{drink}, {drink}, {drink}]
+    #select only the params where quantity is > 1
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:party_id, :item_id)
-    params.require(:order).permit(:drinks)
+    params.require(:order).permit(:drinks => [:id, :quantity], :meals => [:id, :quantity], :desserts => [:id, :quantity])
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,18 +1,4 @@
 class Order < ActiveRecord::Base
   belongs_to :party
   belongs_to :item
-
-  def party_status
-    party = Party.find params[:id]
-    status = party.status
-    if @status == 1
-      render :drinks
-    elsif @status == 2
-      render :meals
-    elsif @status == 3
-      render :desserts
-    end
-  end
-
-
 end
