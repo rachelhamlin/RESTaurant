d035cff252af059e94ca43d1f60b5f517b4996b9
Rachel Hamlin <rachelmhamlin@gmail.com>
2016-04-24 09:58:32 -0400

    fixes party_id in meals, desserts form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,47 +6,21 @@ class OrdersController < ApplicationController
     @drinks = Item.where(category: 'drinks')
     @meals = Item.where(category: 'meals')
     @desserts = Item.where(category: 'desserts')
+    @items = Order.joins(:item)
   end
 
   def create
     @party = Party.find params[:id]
     if @party[:status] == 1
-      @party.drinks.each do |drink|
-        new_order = Hash.new
-        if drink["quantity"].to_i > 0
-          @party = Party.find id
-          new_order["party_id"] = party_id
-          new_order["item_id"] = drink[:id]
-          new_order["quantity"] = drink[:quantity]
-          Order.create new_order
-        end
-      end
+      update_order(order_params[:drinks], params[:id], params[:party_id])
       @party.status = 2
       @party.save
     elsif @party[:status] == 2
-      @meals.each do |meal|
-        new_order = Hash.new
-        if meal["quantity"].to_i > 0
-          @party = Party.find id
-          new_order["party_id"] = party_id
-          new_order["item_id"] = meal[:id]
-          new_order["quantity"] = meal[:quantity]
-          Order.create new_order
-        end
-      end
+      update_order(order_params[:meals], params[:id], params[:party_id])
       @party.status = 3
       @party.save
     elsif @party[:status] == 3
-      @desserts.each do |dessert|
-        new_order = Hash.new
-        if dessert["quantity"].to_i > 0
-          @party = Party.find id
-          new_order["party_id"] = party_id
-          new_order["item_id"] = dessert[:id]
-          new_order["quantity"] = dessert[:quantity]
-          Order.create new_order
-        end
-      end
+      update_order(order_params[:desserts], params[:id], params[:party_id])
       @party.status = 4
       @party.save
     end
@@ -55,18 +29,18 @@ class OrdersController < ApplicationController
 
   private
 
-  # def update_order(foods_hash, id, party_id)
-  #   foods_hash.each do |food_item|
-  #     new_order = Hash.new
-  #     if food_item["quantity"].to_i > 0
-  #       @party = Party.find id
-  #       new_order["party_id"] = party_id
-  #       new_order["item_id"] = food_item[:id]
-  #       new_order["quantity"] = food_item[:quantity]
-  #       Order.create new_order
-  #     end
-  #   end
-  # end
+  def update_order(foods_hash, id, party_id)
+    foods_hash.each do |food_item|
+      new_order = Hash.new
+      if food_item["quantity"].to_i > 0
+        @party = Party.find id
+        new_order["party_id"] = party_id
+        new_order["item_id"] = food_item[:id]
+        new_order["quantity"] = food_item[:quantity]
+        Order.create new_order
+      end
+    end
+  end
 
   def order_params
     params.require(:order).permit(:id, :drinks => [:id, :quantity], :meals => [:id, :quantity], :desserts => [:id, :quantity])
